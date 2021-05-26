945956f1b14be54c66850909e42446016e755089
Alexsander <aemgbo@gmail.com>
2021-05-25 18:59:31 -0700

    implement SignUp

--- InstagramClone/Controller/LoginController.swift
+++ InstagramClone/Controller/LoginController.swift
@@ -14,7 +14,21 @@ import UIKit
 
 class LoginController: UIViewController {
     
-  
+    let logoView: UIView  = {
+       let view = UIView()
+       let logoImageView = UIImageView(image:#imageLiteral(resourceName: "logo2"))
+       logoImageView.contentMode = .scaleAspectFill
+        
+            view.addSubview(logoImageView)
+               logoImageView.anchor(top: nil, left: nil, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 200, height: 50)
+              
+              logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+              logoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+              view.backgroundColor = UIColor(red: 0/255, green: 120/255, blue: 175/255, alpha: 1)
+                  return view
+    }()
+    
+    
 
     let emailTextField: UITextField = {
      let tf = UITextField()
@@ -24,16 +38,70 @@ class LoginController: UIViewController {
      tf.font = UIFont.systemFont(ofSize: 14)
      return tf
  }()
+    
+    let PasswordTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Email"
+        tf.backgroundColor =  UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        return tf
+    }()
+      
+      let LoginButton: UIButton = {
+          let button = UIButton(type: .system)
+          button.setTitle ("Login", for: .normal)
+          button.setTitleColor(.white, for: .normal)
+          button.backgroundColor = UIColor(red: 51/255, green: 153/255, blue: 255/255, alpha: 1)
+          button.layer.cornerRadius = 10
+         
+          return button
+      }()
+        
+      
+      let dontHaveAccountButton: UIButton = {
+          let button = UIButton(type: .system)
+          
+          let attributedTitle = NSMutableAttributedString(string: "Don't have an account?  ", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14), NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+          attributedTitle.append(NSAttributedString(string: "Sign Up", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 14), NSAttributedString.Key.foregroundColor: UIColor(red: 17/255, green: 154/255, blue: 237/255, alpha: 1)]))
+        
+        button.addTarget(self, action: #selector(GoSignUP), for: .touchUpInside)
+          
+        button.setAttributedTitle(attributedTitle, for: .normal
+        )
+          
+          return button
+      }()
    
+    @objc func  GoSignUP(){
+        
+        let SignUp = SignUpController()
+         navigationController?.pushViewController( SignUp, animated: true)
+    }
    override func viewDidLoad() {
-       super.viewDidLoad()
+    super.viewDidLoad()
     self.view.backgroundColor = .white
-    view.addSubview(emailTextField)
+    
+    navigationController?.navigationBar.isHidden = true
+    
+    view.addSubview(logoView)
+    
+    logoView.anchor(top:view.topAnchor, left: view.leftAnchor,  bottom: nil , right : view.rightAnchor , paddingTop:  0  , paddingLeft:  0 , paddingBottom: 0 , paddingRight: 0 , width: 0 , height: 150 )
+
+    
+     let stackview = UIStackView(arrangedSubviews:[ emailTextField, PasswordTextField , LoginButton])
+    
+    stackview.axis         = .vertical
+    stackview.spacing      = 10
+    stackview.distribution = .fillEqually
     
     
+    view.addSubview(stackview)
+    view.addSubview(dontHaveAccountButton)
     
-    emailTextField.anchor(top: view.topAnchor, left: view.leftAnchor,  bottom: nil , right : view.rightAnchor , paddingTop:  40  , paddingLeft:  20 , paddingBottom: 0 , paddingRight: 0 , width: 0 , height: 40 )
-       
+      stackview.anchor(top: logoView.bottomAnchor, left: view.leftAnchor,  bottom: nil , right : view.rightAnchor , paddingTop:  40  , paddingLeft:  40 , paddingBottom: 0 , paddingRight: 40 , width: 0 , height: 140 )
+    
+     dontHaveAccountButton.anchor(top: nil, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 50)
 
     
    }
